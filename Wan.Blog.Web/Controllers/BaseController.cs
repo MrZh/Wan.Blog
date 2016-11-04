384a6d2dd4f313a0a0b506644f2ffd3aeaafbefe
Lucifer <530622110@qq.com>
2016-11-04 13:28:37 +0800

    删除不必要引用

--- Wan.Blog.Web/Controllers/BaseController.cs
+++ Wan.Blog.Web/Controllers/BaseController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Web;
-using System.Web.DynamicData;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Wan.Blog.Domain.Common;
 using Wan.Blog.Domain.Entities;
 
